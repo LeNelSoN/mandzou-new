ddf8fbf7f016097e380557a7134e400e79c72bfa
LeNelSoN <nelis.valentin@gmail.com>
2022-09-04 18:58:47 +0200

    Deploy

--- src/Screen/Landing.jsx
+++ src/Screen/Landing.jsx
@@ -1,16 +1,28 @@
-import React from 'react'
-import Card from '../components/Card'
-import Carousel from '../components/Carousel'
+import React from "react";
+import Card from "../components/Card";
+import Carousel from "../components/Carousel";
+import Mandine from '../assets/Photos Corporate/Mandzou 2021 Photo Corporate Atelier HD (42).jpg'
+
 
 function Landing() {
   return (
     <>
       <Carousel />
-      <div className='container'>
-        <Card />
+      <div className="container">
+        <Card
+          title={"Mandzou"}
+          img={Mandine}
+          description={`Je réalise des accessoires de coiffures sous forme de pince, de
+              serre-tête, de bandeaux / headband, barrette. Je réalise également
+              des bijoux. Chaque pièce est unique et réalisée par mes soins, à
+              la main ou avec une machine à coudre. Des pièces déjà réalisées
+              sont disponibles dans ma boutique en ligne. Vous pouvez également
+              commander sur mesure en me contactant par mail. Et enfin,
+              retrouvez-moi lors de salons, festivals ou expositions.`}
+        />
       </div>
     </>
-  )
+  );
 }
 
-export default Landing
+export default Landing;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,14 @@
-import Mandine from '../assets/Photos Corporate/Mandzou 2021 Photo Corporate Atelier HD (42).jpg'
 
 import React, { useEffect, useState } from "react";
 
-const Card = ({isReverse = false}) => {
+const Card = ({title, description, img, isReverse = false}) => {
     const [LgScreen, setLgScreen] = useState(true)
     
     useEffect(() => {
         const clientWidth = document.documentElement.clientWidth
         console.log('client width : ' + clientWidth)
 
-        LgScreen < 768 ? setLgScreen(false) : setLgScreen(true)
+        clientWidth < 768 ? setLgScreen(false) : setLgScreen(true)
     }, [LgScreen])
 
 
@@ -18,23 +17,17 @@ const Card = ({isReverse = false}) => {
       <div className={`row g-0 m-5 ${isReverse && 'd-flex flex-row-reverse'}`}>
         <div className="col-lg-8 d-flex align-items-center">
           <div className={`card-body mb-3 ${isReverse ? 'ms-lg-5' : 'me-lg-5'} `}>
-            <h5 className="card-title mb-3">Mandzou</h5>
+            <h5 className="card-title mb-3">{title}</h5>
             <small className="card-text">
-              Je réalise des accessoires de coiffures sous forme de pince, de
-              serre-tête, de bandeaux / headband, barrette. Je réalise également
-              des bijoux. Chaque pièce est unique et réalisée par mes soins, à
-              la main ou avec une machine à coudre. Des pièces déjà réalisées
-              sont disponibles dans ma boutique en ligne. Vous pouvez également
-              commander sur mesure en me contactant par mail. Et enfin,
-              retrouvez-moi lors de salons, festivals ou expositions.
+              {description}
             </small>
           </div>
         </div>
         <div className="col-lg-4">
           <img
-            src={Mandine}
+            src={img}
             className={`${LgScreen ? 'w-3' : 'img-fluid'} shadow`}
-            alt="..."
+            alt={img}
           />
         </div>
       </div>
