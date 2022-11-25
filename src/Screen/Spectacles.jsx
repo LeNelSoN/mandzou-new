96447e5f3176e78ad895c3e401f26df974fc1d6b
LeNelSoN <nelis.valentin@gmail.com>
2022-11-25 21:15:32 +0100

    Card présentation et Texte Spectacles screen

--- src/Screen/Spectacles.jsx
+++ src/Screen/Spectacles.jsx
@@ -4,12 +4,17 @@ import Néant from '../assets/Spectacles/Néant.jpg'
 import ofTheMoon from '../assets/Spectacles/Moon.jpg'
 import Déchronologues from '../assets/Spectacles/Déchronologues.jpg'
 import BohémianKaravan from '../assets/Spectacles/Sabbat.jpg'
+import Card from '../components/Card'
+import MandzouSpectacle from '../assets/Photos Corporate/Mandzou 2021 Photo Corporate Atelier HD (9).jpg'
 
 function Spectacles() {
   return (
-    <>
+    <div className='container'>
       <PhotoGrid photo1={Néant} photo2={ofTheMoon} photo3={BohémianKaravan} photo4={Déchronologues}/>
-    </>
+      <Card img={MandzouSpectacle} title={'Spectacle'} description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}/>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
   )
 }
 
