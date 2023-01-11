3719da168e4b2f2fea80e8411c5b7612a8375a76
LeNelSoN <nelis.valentin@gmail.com>
2023-01-11 23:30:49 +0100

    feat add banner on wedding screen

---
+++ src/components/Banner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div class="wedding--banner">
+        <p className='text-center'>Chère "Mademoiselle", sur le point de devenir " Madame "</p>
+    </div>
+  )
+}
+
+export default Banner
